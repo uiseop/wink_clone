c0cd39e6d4eafc96a8ec686e936bcaca0f700f10
uiseop <bono521@naver.com>
2023-07-25 18:45:12 +0900

    ADD: Reset CSS 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
+import GlobalStyles from './styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Home />
   </React.StrictMode>,
 );
