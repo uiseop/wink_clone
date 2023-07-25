2f6a20ae345b8133364ff003931167c6464d99fd
uiseop <bono521@naver.com>
2023-07-25 19:05:55 +0900

    ADD: Utils 컴포넌트 제작 및 가운데 정렬

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Utils } from './Header/Utils';
 import { Navigation } from './Header/Navigation';
+import styled from 'styled-components';
 
 export function Header() {
   return (
-    <div id="header">
+    <Wrapper>
       <Utils />
       <Navigation />
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  min-width: 1200px;
+`;
--- src/components/Header/Utils.tsx
+++ src/components/Header/Utils.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export function Utils() {
   return (
-    <div className="util-wrapper">
-      <div className="util-container">
-        <ul className="util">
-          <li>
-            <a href="#">
-              <img src="https://us.wink.co.kr/pc/renewalV3/common/login.png" alt="" />
-              <span>로그인</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="https://us.wink.co.kr/pc/renewalV3/common/signup.png" alt="" />
-              <span>회원가입</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Wrapper>
+      <UtilLists className="util">
+        <li>
+          <a href="#">
+            <UtilImage src="https://us.wink.co.kr/pc/renewalV3/common/login.png" alt="" />
+            <span>로그인</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <UtilImage src="https://us.wink.co.kr/pc/renewalV3/common/signup.png" alt="" />
+            <span>회원가입</span>
+          </a>
+        </li>
+      </UtilLists>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 1440px;
+  display: flex;
+  justify-content: end;
+`;
+
+const UtilLists = styled.ul`
+  display: flex;
+  gap: 35px;
+  align-items: center;
+  margin-top: 27px;
+  flex-wrap: wrap;
+
+  & a {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const UtilImage = styled.img`
+  width: 20px;
+  height: 22px;
+
+  & + span {
+    font-size: 15px;
+    padding-left: 8px;
+  }
+`;
