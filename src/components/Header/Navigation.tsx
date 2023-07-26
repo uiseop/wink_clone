a1cebfcd6c117f6795a08afbccdbd316b0df9fa8
uiseop <bono521@naver.com>
2023-07-26 11:41:49 +0900

    ADD: Nav 컴포넌트의 디자인 요소 추가 및 사용하지 않는 className 제거

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export function Navigation() {
   return (
-    <nav id="gnb">
+    <Nav>
       <ul>
         <li className="main-logo">
           <a href="#">
@@ -13,7 +14,7 @@ export function Navigation() {
             />
           </a>
         </li>
-        <li className="current">
+        <li>
           <a href="#">홈</a>
         </li>
         <li className="">
@@ -21,29 +22,19 @@ export function Navigation() {
           <ul>
             <div>
               <li>
-                <a className="" href="#">
-                  수준별 한글 · 국어
-                </a>
+                <a href="#">수준별 한글 · 국어</a>
               </li>
               <li>
-                <a className="" href="#">
-                  수준별 수학 · 연산
-                </a>
+                <a href="#">수준별 수학 · 연산</a>
               </li>
               <li>
-                <a className="" href="#">
-                  수준별 영어&amp;파닉스
-                </a>
+                <a href="#">수준별 영어&amp;파닉스</a>
               </li>
               <li>
-                <a className="" href="#">
-                  초등 교과 학습(학교 공부 대비)
-                </a>
+                <a href="#">초등 교과 학습(학교 공부 대비)</a>
               </li>
               <li>
-                <a className="" href="#">
-                  선택활동(배경지식)
-                </a>
+                <a href="#">선택활동(배경지식)</a>
               </li>
             </div>
           </ul>
@@ -53,14 +44,10 @@ export function Navigation() {
           <ul>
             <div>
               <li>
-                <a className="" href="#">
-                  유료 학습 신청
-                </a>
+                <a href="#">유료 학습 신청</a>
               </li>
               <li>
-                <a className="" href="#">
-                  무료 체험학습 신청
-                </a>
+                <a href="#">무료 체험학습 신청</a>
               </li>
             </div>
           </ul>
@@ -73,114 +60,69 @@ export function Navigation() {
           <ul>
             <div>
               <li>
-                <a className="" href="#">
-                  공지사항
-                </a>
+                <a href="#">공지사항</a>
               </li>
               <li>
-                <a className="" href="#">
-                  이벤트
-                </a>
+                <a href="#">이벤트</a>
               </li>
               <li>
-                <a className="" href="#">
-                  자주하는 질문
-                </a>
+                <a href="#">자주하는 질문</a>
               </li>
               <li>
-                <a className="" href="#">
-                  신고 · 건의 · 문의
-                </a>
+                <a href="#">신고 · 건의 · 문의</a>
               </li>
               <li>
-                <a className="" href="#">
-                  학부모 앱 다운로드
-                </a>
+                <a href="#">학부모 앱 다운로드</a>
               </li>
               <li>
-                <a className="" href="#">
-                  윙크 선생님 모집
-                </a>
+                <a href="#">윙크 선생님 모집</a>
               </li>
               <li>
-                <a className="" href="#">
-                  홍보 선생님 모집
-                </a>
+                <a href="#">홍보 선생님 모집</a>
               </li>
             </div>
           </ul>
         </li>
       </ul>
-      <div className="lay-floating-wrapper">
-        <div className="lay-floating">
-          <div className="lay-close-button" id="floating-close">
-            <img
-              src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/open_221019.png"
-              alt=""
-            />
-          </div>
-          <div className="lay-benefit-image">
-            <img
-              src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/benefit_230705.png"
-              alt=""
-            />
-          </div>
-          <div className="lay-free-study" id="floating-free-apply">
-            <img
-              src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/free_study_blink_230419_v2.png"
-              alt=""
-            />
-          </div>
-          <div className="lay-additional-info">
-            <hr className="lay-floating-divider" />
-            <div className="lay-reference-room">
-              <img
-                src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/reference_room_221019.png"
-                alt=""
-              />
-            </div>
-            <div className="lay-reference-room-button">
-              <img
-                src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/reference_room_button_b.png"
-                alt=""
-              />
-            </div>
-            <hr className="lay-floating-divider" />
-            <div className="lay-download-parent-app" id="floating-download-parent-app">
-              <img
-                src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/app_download_221110.png"
-                alt=""
-              />
-            </div>
-            <div className="lay-recruit-teacher" id="floating-download-recruit-teacher">
-              <img
-                src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/recruit_221110.png"
-                alt=""
-              />
-            </div>
-            <div className="lay-recruit-promotion-teacher">
-              <img
-                src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/recruit_promotion_230419_v2.png"
-                alt=""
-              />
-            </div>
-            <div className="lay-happy-talk">
-              <img
-                src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/chatting_221110.png"
-                alt=""
-                id="floating-happy-talk"
-              />
-            </div>
-            <hr className="lay-floating-divider" />
-            <div className="lay-cs-center">
-              <img
-                src="https://s.wink.co.kr/pc/renewalV3/floating_right_banner/cs_center_221019.png"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    </Nav>
   );
 }
+
+const Nav = styled.nav`
+  min-width: 1920px;
+  height: 75px;
+  background-color: white;
+  position: relative;
+  z-index: 100;
+  border-bottom: 1px solid #d7d7d7;
+  display: flex;
+  justify-content: center;
+
+  & > ul {
+    display: flex;
+    width: 1440px;
+    justify-content: center;
+    align-items: center;
+
+    & li {
+      width: 240px;
+      position: relative;
+      text-align: center;
+
+      & > ul {
+        position: absolute;
+        left: 0;
+        top: 100%;
+        max-height: 0;
+        overflow: hidden;
+        width: 100%;
+        margin: 0;
+        transition: max-height 0.5s;
+      }
+
+      //&.on > ul {
+      //  max-height: 400px;
+      //}
+    }
+  }
+`;
