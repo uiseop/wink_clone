5a4b441eecb5665349cdea03dbb8fffe59ca144c
uiseop <bono521@naver.com>
2023-07-26 11:51:40 +0900

    ADD: gnb(Navigationbar) 기능을 구현하기 위해 react-router-dom을 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from '@pages/Home';
 import GlobalStyles from '@styles/GlobalStyles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
